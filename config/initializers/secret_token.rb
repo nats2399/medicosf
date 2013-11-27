89aa3f55b88e2aa411ed6a76886954a19db7feb4
Nathalie Ayala Santana <nathalieaysa@gmail.com>
2013-11-27 09:46:31 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medicosf::Application.config.secret_key_base = '38b034a60953a8d63876af50076befdcac7231a23c586f7a6c2a69500be20eb8ad629170bed8ad97cf1ffb82dae2b31720d5c84623ba5b36c109c1a36a3a9701'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Medicosf::Application.config.session_store :cookie_store, key: '_medicosf_session'
